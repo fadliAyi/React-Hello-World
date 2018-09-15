8396876747b0b2bca94ec860298da6795db6bbe6
unknown <m.fadli4545@gmail.com>
2018-09-15 17:49:07 +0800

    new change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Muhammad Fadli</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            project ini untuk pembelajaran react app 
         </p>
       </div>
     );
